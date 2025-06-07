2495456c5d955cfb9d0113888dce21dfa60b13ca
stanoz <s.oziemczuk@gmail.com>
2025-06-07 19:24:41 +0200

    close Redis client after all tests in unit.test.js

--- tests/unit/unit.test.js
+++ tests/unit/unit.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
 const pool = require('../../db.js')
+const redisClient = require('../../redisClient')
 
 describe('POST /texts', () => {
   afterAll(async () => {
     await pool.end();
+    await redisClient.quit();
   });
 
   it('should return 400 if text is not provided', async () => {
