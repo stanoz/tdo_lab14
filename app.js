b1982fb51aa01a81723f3a4a79e2e4178b211381
stanoz <s.oziemczuk@gmail.com>
2025-06-07 16:53:50 +0200

    change default server port from 3001 to 3000 and restore .env file creation in GitHub Actions workflow

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use('/texts', textsRouter);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
