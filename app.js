68f55e4b58dacd22246b15dcb0331357d0932e90
stanoz <s.oziemczuk@gmail.com>
2025-06-07 19:18:37 +0200

    close app after all tests in unit.test.js

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 app.use('/texts', textsRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = app;
