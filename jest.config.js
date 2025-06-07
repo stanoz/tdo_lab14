d0e319b8bdf531d43b22d53d2c8ad8e2a3c0b519
stanoz <s.oziemczuk@gmail.com>
2025-06-07 20:46:12 +0200

    feat: update Jest configuration to include junit reporter and adjust coverage report paths

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
-const nodeVer = process.env.NODE_VERSION || process.version.replace('v','');
-
-module.exports = {
-    collectCoverage: true,
-    coverageDirectory: `coverage/node-${nodeVer}`,
-    coverageReporters: ['lcov', 'text'],
-};
+// const nodeVer = process.env.NODE_VERSION || process.version.replace('v','');
+//
+// module.exports = {
+//     collectCoverage: true,
+//     coverageDirectory: `coverage/node-${nodeVer}`,
+//     coverageReporters: ['lcov', 'text'],
+// };
